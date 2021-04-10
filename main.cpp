2a0a3bb90fa406d5f3cd997a268b554df01717cf
Thanh <quang.thanh.ta@pr1meti.de>
2021-04-10 18:57:56 +0200

    feat: Add OLED output

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
-//#include <cstdio>
 #include <pico/printf.h>
 #include <sstream>
 #include <iomanip>
-//#include <stdio.h>
+#include "RPi-Pico-SSD1306-library/GFX.hpp"
 #include "pico/stdlib.h" // NOLINT(modernize-deprecated-headers)
 #include "pico/util/datetime.h"
 #include "hardware/rtc.h"
 #include "util/DateHandler.h"
-#include "SSD1306.hpp"
 
 static const char *DATETIME_DOWS[7] = {
         "Sonntag",
@@ -21,6 +19,17 @@ static const char *DATETIME_DOWS[7] = {
 
 int main() {
     stdio_init_all();
+
+    i2c_init(i2c_default, 400'000);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+
+    GFX gfx{0x3C, 128, 64, i2c_default};
+    gfx.drawString(0, 10, "Waiting for input");
+    gfx.display();
+
     printf("Ready when you are!\n");
 
     datetime_t t = getInitialDate();
@@ -32,22 +41,26 @@ int main() {
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
     while (true) {
+        gfx.clear();
         rtc_get_datetime(&t);
 
-        std::ostringstream os{};
+        std::ostringstream os{}, os2{};
 
-        os << "\r"
-           << DATETIME_DOWS[t.dotw] << ", "
-           << std::setw(2) << std::setfill('0') << std::to_string(t.day) << "."
-           << std::setw(2) << std::setfill('0') << std::to_string(t.month) << "."
-           << std::to_string(t.year)
-           << " "
-           << std::setw(2) << std::setfill('0') << std::to_string(t.hour) << ":"
-           << std::setw(2) << std::setfill('0') << std::to_string(t.min) << ":"
-           << std::setw(2) << std::setfill('0') << std::to_string(t.sec)
-           << " ";
+        os
+                << DATETIME_DOWS[t.dotw] << ", "
+                << std::setw(2) << std::setfill('0') << std::to_string(t.day) << "."
+                << std::setw(2) << std::setfill('0') << std::to_string(t.month) << "."
+                << std::to_string(t.year);
+        os2
+                << std::setw(2) << std::setfill('0') << std::to_string(t.hour) << ":"
+                << std::setw(2) << std::setfill('0') << std::to_string(t.min) << ":"
+                << std::setw(2) << std::setfill('0') << std::to_string(t.sec);
 
-        printf("%s", os.str().c_str());
+        printf("\r %s %s", os.str().c_str(), os2.str().c_str());
+        os << "\n" << os2.str();
+        // TODO: figure out how to display multi line text
+        gfx.drawString(0, 10, os.str());
+        gfx.display();
         sleep_ms(1000);
     }
 #pragma clang diagnostic pop
